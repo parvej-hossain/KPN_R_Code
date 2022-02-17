7d6aa1ebce35e116b0da85143eff21439e4ad92b
parvej-hossain <70034779+parvej-hossain@users.noreply.github.com>
2022-02-17 20:23:57 +0600

    Update Stat_analysis.R

--- Stat_analysis.R
+++ Stat_analysis.R
@@ -16,26 +16,8 @@ ggplot(data, aes(y=mean_res, x=mean_vir)) +
                    point.padding = 0.5,max.overlaps=20,
                    segment.color = 'grey50')+ylim(0,2.5)
   
-  
-ggplot(data, aes(y= mean_res, x= mean_vir, colour="green", label=ST,color="blue"))+
-    geom_point(aes(size=data$total)) +geom_text(hjust=0.5, vjust=0)
+ 
 
-#####new data for more analysis
 
-dt<- merged_570_kleb_metadata
-View(dt)
-ggplot(dt,aes(x=dt$AdmissionYear,y=dt$ST))+geom_point()+
-  theme(legend.position="top")
 
- new<-table(dt$AdmissionYear,dt$ST)                  
- View(new)
-install.packages("writexl") 
-library("writexl")
-new<-data.frame(new)
-write_xlsx(new,"/home/chrf/KPN_R_Code/new.xlsx")
-table(dt$ST)
-library(dplyr)
-st<-as.factor(dt$ST)
-dt %>%
-  count(ST) %>%
-  group_by(dt$AdmissionYear) 
+ 
